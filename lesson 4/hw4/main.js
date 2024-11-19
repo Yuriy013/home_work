b5715c75c09ec28838d3c6722debbd7c96e2cad2
Yuriy013 <yuriyvitvitskyy@gmail.com>
2024-11-20 00:32:39 +0100

    lesson 4 hw4

---
+++ lesson 4/hw4/main.js
@@ -0,0 +1,10 @@
+//- створити функцію яка приймає масив та виводить кожен його елемент
+
+function foobar(array) {
+    for (const item of array) {
+        console.log(item)
+
+    }
+    
+}
+foobar()
