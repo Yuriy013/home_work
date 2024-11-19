e29ff0274079fbdeb33086411dfb6410038e2b64
Yuriy013 <yuriyvitvitskyy@gmail.com>
2024-11-19 23:49:43 +0100

    lesson3 hw7

---
+++ lesson 4/hw1/main.js
@@ -0,0 +1,10 @@
+///- створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+function square(a,b) {
+    let result = a * b;
+    console.log(result)
+    return result;
+
+
+}
+square()
