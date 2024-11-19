3d824d4eb6d23cbf7b7b15f701963c6889b38352
Yuriy013 <yuriyvitvitskyy@gmail.com>
2024-11-20 00:24:33 +0100

    lesson 4 hw3

---
+++ lesson 4/hw3/main.js
@@ -0,0 +1,7 @@
+///створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+
+
+function squareCilinderf(radius, height) {
+    return 2 * 3.14 * radius *height
+}
+console.log(squareCilinderf(10,40))
